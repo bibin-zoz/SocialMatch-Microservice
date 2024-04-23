ffb94af901e5112843c4f7728d019cd3132b324a
bibin-zoz <bibinvinodvfx@gmail.com>
2024-04-23 10:13:21 +0530

    intrst and pref

--- pkg/pb/userauth_grpc.pb.go
+++ pkg/pb/userauth_grpc.pb.go
@@ -4,7 +4,7 @@
 // - protoc             v4.25.3
 // source: pkg/pb/userauth.proto
 
-package pb
+package admin
 
 import (
 	context "context"
@@ -19,311 +19,163 @@ import (
 const _ = grpc.SupportPackageIsVersion7
 
 const (
-	User_UserSignUp_FullMethodName          = "/user.User/UserSignUp"
-	User_UserLogin_FullMethodName           = "/user.User/UserLogin"
-	User_UserEditDetails_FullMethodName     = "/user.User/UserEditDetails"
-	User_UserOtpGeneration_FullMethodName   = "/user.User/UserOtpGeneration"
-	User_UserOtpVerification_FullMethodName = "/user.User/UserOtpVerification"
-	User_GetUsers_FullMethodName            = "/user.User/GetUsers"
-	User_UpdateUserStatus_FullMethodName    = "/user.User/UpdateUserStatus"
+	Admin_AdminLogin_FullMethodName     = "/admin.Admin/AdminLogin"
+	Admin_GetInterests_FullMethodName   = "/admin.Admin/GetInterests"
+	Admin_GetPreferences_FullMethodName = "/admin.Admin/GetPreferences"
 )
 
-// UserClient is the client API for User service.
+// AdminClient is the client API for Admin service.
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
-type UserClient interface {
-	UserSignUp(ctx context.Context, in *UserSignUpRequest, opts ...grpc.CallOption) (*UserSignUpResponse, error)
-	UserLogin(ctx context.Context, in *UserLoginRequest, opts ...grpc.CallOption) (*UserLoginResponse, error)
-	UserEditDetails(ctx context.Context, in *UserEditDetailsRequest, opts ...grpc.CallOption) (*UserEditDetailsResponse, error)
-	UserOtpGeneration(ctx context.Context, in *UserOtpRequest, opts ...grpc.CallOption) (*UserOtpRequestResponse, error)
-	UserOtpVerification(ctx context.Context, in *UserOtpVerificationRequest, opts ...grpc.CallOption) (*UserOtpVerificationResponse, error)
-	GetUsers(ctx context.Context, in *GetUsersRequest, opts ...grpc.CallOption) (*GetUsersResponse, error)
-	UpdateUserStatus(ctx context.Context, in *UpdateUserStatusRequest, opts ...grpc.CallOption) (*UpdateUserStatusResponce, error)
+type AdminClient interface {
+	AdminLogin(ctx context.Context, in *AdminLoginRequest, opts ...grpc.CallOption) (*AdminLoginResponse, error)
+	GetInterests(ctx context.Context, in *GetInterestsRequest, opts ...grpc.CallOption) (*GetInterestsResponse, error)
+	GetPreferences(ctx context.Context, in *GetPreferencesRequest, opts ...grpc.CallOption) (*GetPreferencesResponse, error)
 }
 
-type userClient struct {
+type adminClient struct {
 	cc grpc.ClientConnInterface
 }
 
-func NewUserClient(cc grpc.ClientConnInterface) UserClient {
-	return &userClient{cc}
+func NewAdminClient(cc grpc.ClientConnInterface) AdminClient {
+	return &adminClient{cc}
 }
 
-func (c *userClient) UserSignUp(ctx context.Context, in *UserSignUpRequest, opts ...grpc.CallOption) (*UserSignUpResponse, error) {
-	out := new(UserSignUpResponse)
-	err := c.cc.Invoke(ctx, User_UserSignUp_FullMethodName, in, out, opts...)
+func (c *adminClient) AdminLogin(ctx context.Context, in *AdminLoginRequest, opts ...grpc.CallOption) (*AdminLoginResponse, error) {
+	out := new(AdminLoginResponse)
+	err := c.cc.Invoke(ctx, Admin_AdminLogin_FullMethodName, in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-func (c *userClient) UserLogin(ctx context.Context, in *UserLoginRequest, opts ...grpc.CallOption) (*UserLoginResponse, error) {
-	out := new(UserLoginResponse)
-	err := c.cc.Invoke(ctx, User_UserLogin_FullMethodName, in, out, opts...)
+func (c *adminClient) GetInterests(ctx context.Context, in *GetInterestsRequest, opts ...grpc.CallOption) (*GetInterestsResponse, error) {
+	out := new(GetInterestsResponse)
+	err := c.cc.Invoke(ctx, Admin_GetInterests_FullMethodName, in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-func (c *userClient) UserEditDetails(ctx context.Context, in *UserEditDetailsRequest, opts ...grpc.CallOption) (*UserEditDetailsResponse, error) {
-	out := new(UserEditDetailsResponse)
-	err := c.cc.Invoke(ctx, User_UserEditDetails_FullMethodName, in, out, opts...)
+func (c *adminClient) GetPreferences(ctx context.Context, in *GetPreferencesRequest, opts ...grpc.CallOption) (*GetPreferencesResponse, error) {
+	out := new(GetPreferencesResponse)
+	err := c.cc.Invoke(ctx, Admin_GetPreferences_FullMethodName, in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-func (c *userClient) UserOtpGeneration(ctx context.Context, in *UserOtpRequest, opts ...grpc.CallOption) (*UserOtpRequestResponse, error) {
-	out := new(UserOtpRequestResponse)
-	err := c.cc.Invoke(ctx, User_UserOtpGeneration_FullMethodName, in, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
-func (c *userClient) UserOtpVerification(ctx context.Context, in *UserOtpVerificationRequest, opts ...grpc.CallOption) (*UserOtpVerificationResponse, error) {
-	out := new(UserOtpVerificationResponse)
-	err := c.cc.Invoke(ctx, User_UserOtpVerification_FullMethodName, in, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
-func (c *userClient) GetUsers(ctx context.Context, in *GetUsersRequest, opts ...grpc.CallOption) (*GetUsersResponse, error) {
-	out := new(GetUsersResponse)
-	err := c.cc.Invoke(ctx, User_GetUsers_FullMethodName, in, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
-func (c *userClient) UpdateUserStatus(ctx context.Context, in *UpdateUserStatusRequest, opts ...grpc.CallOption) (*UpdateUserStatusResponce, error) {
-	out := new(UpdateUserStatusResponce)
-	err := c.cc.Invoke(ctx, User_UpdateUserStatus_FullMethodName, in, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
-// UserServer is the server API for User service.
-// All implementations must embed UnimplementedUserServer
+// AdminServer is the server API for Admin service.
+// All implementations must embed UnimplementedAdminServer
 // for forward compatibility
-type UserServer interface {
-	UserSignUp(context.Context, *UserSignUpRequest) (*UserSignUpResponse, error)
-	UserLogin(context.Context, *UserLoginRequest) (*UserLoginResponse, error)
-	UserEditDetails(context.Context, *UserEditDetailsRequest) (*UserEditDetailsResponse, error)
-	UserOtpGeneration(context.Context, *UserOtpRequest) (*UserOtpRequestResponse, error)
-	UserOtpVerification(context.Context, *UserOtpVerificationRequest) (*UserOtpVerificationResponse, error)
-	GetUsers(context.Context, *GetUsersRequest) (*GetUsersResponse, error)
-	UpdateUserStatus(context.Context, *UpdateUserStatusRequest) (*UpdateUserStatusResponce, error)
-	mustEmbedUnimplementedUserServer()
+type AdminServer interface {
+	AdminLogin(context.Context, *AdminLoginRequest) (*AdminLoginResponse, error)
+	GetInterests(context.Context, *GetInterestsRequest) (*GetInterestsResponse, error)
+	GetPreferences(context.Context, *GetPreferencesRequest) (*GetPreferencesResponse, error)
+	mustEmbedUnimplementedAdminServer()
 }
 
-// UnimplementedUserServer must be embedded to have forward compatible implementations.
-type UnimplementedUserServer struct {
+// UnimplementedAdminServer must be embedded to have forward compatible implementations.
+type UnimplementedAdminServer struct {
 }
 
-func (UnimplementedUserServer) UserSignUp(context.Context, *UserSignUpRequest) (*UserSignUpResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method UserSignUp not implemented")
+func (UnimplementedAdminServer) AdminLogin(context.Context, *AdminLoginRequest) (*AdminLoginResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method AdminLogin not implemented")
 }
-func (UnimplementedUserServer) UserLogin(context.Context, *UserLoginRequest) (*UserLoginResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method UserLogin not implemented")
+func (UnimplementedAdminServer) GetInterests(context.Context, *GetInterestsRequest) (*GetInterestsResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetInterests not implemented")
 }
-func (UnimplementedUserServer) UserEditDetails(context.Context, *UserEditDetailsRequest) (*UserEditDetailsResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method UserEditDetails not implemented")
+func (UnimplementedAdminServer) GetPreferences(context.Context, *GetPreferencesRequest) (*GetPreferencesResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetPreferences not implemented")
 }
-func (UnimplementedUserServer) UserOtpGeneration(context.Context, *UserOtpRequest) (*UserOtpRequestResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method UserOtpGeneration not implemented")
-}
-func (UnimplementedUserServer) UserOtpVerification(context.Context, *UserOtpVerificationRequest) (*UserOtpVerificationResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method UserOtpVerification not implemented")
-}
-func (UnimplementedUserServer) GetUsers(context.Context, *GetUsersRequest) (*GetUsersResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method GetUsers not implemented")
-}
-func (UnimplementedUserServer) UpdateUserStatus(context.Context, *UpdateUserStatusRequest) (*UpdateUserStatusResponce, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method UpdateUserStatus not implemented")
-}
-func (UnimplementedUserServer) mustEmbedUnimplementedUserServer() {}
+func (UnimplementedAdminServer) mustEmbedUnimplementedAdminServer() {}
 
-// UnsafeUserServer may be embedded to opt out of forward compatibility for this service.
-// Use of this interface is not recommended, as added methods to UserServer will
+// UnsafeAdminServer may be embedded to opt out of forward compatibility for this service.
+// Use of this interface is not recommended, as added methods to AdminServer will
 // result in compilation errors.
-type UnsafeUserServer interface {
-	mustEmbedUnimplementedUserServer()
+type UnsafeAdminServer interface {
+	mustEmbedUnimplementedAdminServer()
 }
 
-func RegisterUserServer(s grpc.ServiceRegistrar, srv UserServer) {
-	s.RegisterService(&User_ServiceDesc, srv)
+func RegisterAdminServer(s grpc.ServiceRegistrar, srv AdminServer) {
+	s.RegisterService(&Admin_ServiceDesc, srv)
 }
 
-func _User_UserSignUp_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(UserSignUpRequest)
+func _Admin_AdminLogin_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(AdminLoginRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(UserServer).UserSignUp(ctx, in)
+		return srv.(AdminServer).AdminLogin(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: User_UserSignUp_FullMethodName,
+		FullMethod: Admin_AdminLogin_FullMethodName,
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(UserServer).UserSignUp(ctx, req.(*UserSignUpRequest))
+		return srv.(AdminServer).AdminLogin(ctx, req.(*AdminLoginRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-func _User_UserLogin_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(UserLoginRequest)
+func _Admin_GetInterests_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(GetInterestsRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(UserServer).UserLogin(ctx, in)
+		return srv.(AdminServer).GetInterests(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: User_UserLogin_FullMethodName,
+		FullMethod: Admin_GetInterests_FullMethodName,
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(UserServer).UserLogin(ctx, req.(*UserLoginRequest))
+		return srv.(AdminServer).GetInterests(ctx, req.(*GetInterestsRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-func _User_UserEditDetails_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(UserEditDetailsRequest)
+func _Admin_GetPreferences_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(GetPreferencesRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(UserServer).UserEditDetails(ctx, in)
+		return srv.(AdminServer).GetPreferences(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: User_UserEditDetails_FullMethodName,
+		FullMethod: Admin_GetPreferences_FullMethodName,
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(UserServer).UserEditDetails(ctx, req.(*UserEditDetailsRequest))
+		return srv.(AdminServer).GetPreferences(ctx, req.(*GetPreferencesRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-func _User_UserOtpGeneration_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(UserOtpRequest)
-	if err := dec(in); err != nil {
-		return nil, err
-	}
-	if interceptor == nil {
-		return srv.(UserServer).UserOtpGeneration(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: User_UserOtpGeneration_FullMethodName,
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(UserServer).UserOtpGeneration(ctx, req.(*UserOtpRequest))
-	}
-	return interceptor(ctx, in, info, handler)
-}
-
-func _User_UserOtpVerification_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(UserOtpVerificationRequest)
-	if err := dec(in); err != nil {
-		return nil, err
-	}
-	if interceptor == nil {
-		return srv.(UserServer).UserOtpVerification(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: User_UserOtpVerification_FullMethodName,
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(UserServer).UserOtpVerification(ctx, req.(*UserOtpVerificationRequest))
-	}
-	return interceptor(ctx, in, info, handler)
-}
-
-func _User_GetUsers_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(GetUsersRequest)
-	if err := dec(in); err != nil {
-		return nil, err
-	}
-	if interceptor == nil {
-		return srv.(UserServer).GetUsers(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: User_GetUsers_FullMethodName,
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(UserServer).GetUsers(ctx, req.(*GetUsersRequest))
-	}
-	return interceptor(ctx, in, info, handler)
-}
-
-func _User_UpdateUserStatus_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(UpdateUserStatusRequest)
-	if err := dec(in); err != nil {
-		return nil, err
-	}
-	if interceptor == nil {
-		return srv.(UserServer).UpdateUserStatus(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: User_UpdateUserStatus_FullMethodName,
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(UserServer).UpdateUserStatus(ctx, req.(*UpdateUserStatusRequest))
-	}
-	return interceptor(ctx, in, info, handler)
-}
-
-// User_ServiceDesc is the grpc.ServiceDesc for User service.
+// Admin_ServiceDesc is the grpc.ServiceDesc for Admin service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
-var User_ServiceDesc = grpc.ServiceDesc{
-	ServiceName: "user.User",
-	HandlerType: (*UserServer)(nil),
+var Admin_ServiceDesc = grpc.ServiceDesc{
+	ServiceName: "admin.Admin",
+	HandlerType: (*AdminServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
-			MethodName: "UserSignUp",
-			Handler:    _User_UserSignUp_Handler,
+			MethodName: "AdminLogin",
+			Handler:    _Admin_AdminLogin_Handler,
 		},
 		{
-			MethodName: "UserLogin",
-			Handler:    _User_UserLogin_Handler,
+			MethodName: "GetInterests",
+			Handler:    _Admin_GetInterests_Handler,
 		},
 		{
-			MethodName: "UserEditDetails",
-			Handler:    _User_UserEditDetails_Handler,
-		},
-		{
-			MethodName: "UserOtpGeneration",
-			Handler:    _User_UserOtpGeneration_Handler,
-		},
-		{
-			MethodName: "UserOtpVerification",
-			Handler:    _User_UserOtpVerification_Handler,
-		},
-		{
-			MethodName: "GetUsers",
-			Handler:    _User_GetUsers_Handler,
-		},
-		{
-			MethodName: "UpdateUserStatus",
-			Handler:    _User_UpdateUserStatus_Handler,
+			MethodName: "GetPreferences",
+			Handler:    _Admin_GetPreferences_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
