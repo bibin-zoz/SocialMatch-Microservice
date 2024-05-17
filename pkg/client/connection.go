4b5c5228afbecc6dc3b7952c068ed689f01159d3
bibin-zoz <bibinvinodvfx@gmail.com>
2024-05-17 13:15:54 +0530

    video call working front end

---
+++ pkg/client/connection.go
@@ -0,0 +1,45 @@
+package client
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/bibin-zoz/api-gateway/pkg/client/interfaces"
+	"github.com/bibin-zoz/api-gateway/pkg/config"
+	Connection "github.com/bibin-zoz/api-gateway/pkg/pb/Connections"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type ConnectionClient struct {
+	// Client     pb.ConnectionServiceClient
+	ConnectionClient Connection.ConnectionServiceClient
+}
+
+func NewConnectionServiceClient(cfg config.Config) interfaces.ConnectionClient {
+	ConnectionGrpcConnection, err := grpc.Dial(cfg.ConnectionSvcUrl, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		fmt.Println("Could not connect to Connection service", err)
+		return nil
+	}
+	ConnectionGrpcClient := Connection.NewConnectionServiceClient(ConnectionGrpcConnection)
+	return &ConnectionClient{
+
+		ConnectionClient: ConnectionGrpcClient,
+	}
+
+}
+func (cc *ConnectionClient) CheckUserConnection(userID int32, receiverID int32) (bool, error) {
+	req := &Connection.CheckUserConnectionReq{
+		UserID:     userID,
+		ReceiverID: receiverID,
+	}
+
+	resp, err := cc.ConnectionClient.CheckUserConnection(context.Background(), req)
+	if err != nil {
+		return false, fmt.Errorf("failed to check user connection: %v", err)
+	}
+
+	return resp.Connected, nil
+}
