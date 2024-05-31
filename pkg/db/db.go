57c365e2ec9cff1438603576828200cc4a02a1a8
bibin-zoz <bibinvinodvfx@gmail.com>
2024-05-31 10:00:16 +0530

    auto create database if not exist

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -3,6 +3,7 @@ package db
 import (
 	"context"
 	"fmt"
+	"log"
 
 	"github.com/bibin-zoz/social-match-userauth-svc/pkg/config"
 	"github.com/bibin-zoz/social-match-userauth-svc/pkg/domain"
@@ -15,26 +16,57 @@ import (
 // ConnectDatabase establishes connections to both PostgreSQL and MongoDB databases
 func ConnectDatabase(cfg config.Config) (*gorm.DB, *mongo.Database, error) {
 	// PostgreSQL connection
-	psqlInfo := fmt.Sprintf("host=%s user=%s dbname=%s port=%s password=%s", cfg.DBHost, cfg.DBUser, cfg.DBName, cfg.DBPort, cfg.DBPassword)
+	psqlInfo := fmt.Sprintf("host=%s user=%s password=%s port=%s sslmode=disable", cfg.DBHost, cfg.DBUser, cfg.DBPassword, cfg.DBPort)
 	db, dbErr := gorm.Open(postgres.Open(psqlInfo), &gorm.Config{
 		SkipDefaultTransaction: true,
 	})
+	if dbErr != nil {
+		return nil, nil, dbErr
+	}
+
+	dbName := cfg.DBName
+	var exists bool
+
+	// Check if the database exists
+	err := db.Raw("SELECT EXISTS (SELECT FROM pg_database WHERE datname = ?)", dbName).Scan(&exists).Error
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// Create the database if it does not exist
+	if !exists {
+		err = db.Exec(fmt.Sprintf("CREATE DATABASE %s", dbName)).Error
+		if err != nil {
+			return nil, nil, err
+		}
+		log.Println("Created database " + dbName)
+	}
+
+	// Connect to the newly created database
+	db, err = gorm.Open(postgres.Open(psqlInfo+" dbname="+dbName), &gorm.Config{})
+	if err != nil {
+		return nil, nil, err
+	}
 
 	// MongoDB connection
-	mongoURI := fmt.Sprintf("mongodb://%s:%s/%s", cfg.MongoDBHost, cfg.MongoDBPort, cfg.MongoDBDatabase)
-	fmt.Println(mongoURI, cfg.MongoDBHost)
+	mongoURI := fmt.Sprintf("mongodb://%s:%s", cfg.MongoDBHost, cfg.MongoDBPort)
 	mongoClient, err := mongo.Connect(context.Background(), options.Client().ApplyURI(mongoURI))
 	if err != nil {
 		return nil, nil, err
 	}
 
-	// Check the connection
+	// Check the MongoDB connection
 	err = mongoClient.Ping(context.Background(), nil)
 	if err != nil {
 		return nil, nil, err
 	}
 	mongoDB := mongoClient.Database(cfg.MongoDBDatabase)
-	db.AutoMigrate(&domain.User{}, &domain.UserInterest{}, &domain.UserPreference{}, &domain.Connections{}, &domain.UserMessage{}, &domain.Media{})
 
-	return db, mongoDB, dbErr
+	// AutoMigrate the domain models
+	err = db.AutoMigrate(&domain.User{}, &domain.UserInterest{}, &domain.UserPreference{}, &domain.Connections{}, &domain.UserMessage{}, &domain.Media{})
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return db, mongoDB, nil
 }
