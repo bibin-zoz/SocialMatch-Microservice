4b5e05a86f0ec6879cc2b768a29e4562eab0a07d
bibin-zoz <bibinvinodvfx@gmail.com>
2024-06-01 10:20:00 +0530

    swagger notation added ..swagger updated

--- pkg/api/handler/user_friends.go
+++ pkg/api/handler/user_friends.go
@@ -8,6 +8,16 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// UserFollow godoc
+// @Summary Follow a user
+// @Description Allows a user to follow another user
+// @Tags Users
+// @Accept json
+// @Produce json
+// @Param user_id body int true "User ID to follow"
+// @Success 201 {object} map[string]interface{}
+// @Failure 400 {object} map[string]interface{}
+// @Router /user/follow [post]
 func (ur *UserHandler) UserFollow(c *gin.Context) {
 	var req struct {
 		Userid int `json:"user_id"`
@@ -35,6 +45,17 @@ func (ur *UserHandler) UserFollow(c *gin.Context) {
 	success := response.ClientResponse(http.StatusCreated, "successfully followed user", nil, nil)
 	c.JSON(http.StatusCreated, success)
 }
+
+// BlockUser godoc
+// @Summary Block a user
+// @Description Allows a user to block another user
+// @Tags Users
+// @Accept json
+// @Produce json
+// @Param user_id body int true "User ID to block"
+// @Success 201 {object} map[string]interface{}
+// @Failure 400 {object} map[string]interface{}
+// @Router /user/block [post]
 func (ur *UserHandler) BlockUser(c *gin.Context) {
 	var req struct {
 		Userid int `json:"user_id"`
@@ -62,6 +83,16 @@ func (ur *UserHandler) BlockUser(c *gin.Context) {
 	success := response.ClientResponse(http.StatusCreated, "successfully Blocked user", req.Userid, nil)
 	c.JSON(http.StatusCreated, success)
 }
+
+// GetConnections godoc
+// @Summary Get user connections
+// @Description Retrieves a user's connections
+// @Tags Users
+// @Accept json
+// @Produce json
+// @Success 200 {object} map[string]interface{}
+// @Failure 400 {object} map[string]interface{}
+// @Router /user/connections [get]
 func (ur *UserHandler) GetConnections(c *gin.Context) {
 	authHeader := c.GetHeader("Authorization")
 	token := helper.GetTokenFromHeader(authHeader)
